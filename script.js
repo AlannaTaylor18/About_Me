04a2a651bf0816c4408da4d67419536ccf200360
AlannaTaylor18 <alannataylor@live.com>
2025-06-09 16:32:35 -0400

    Update recommendation textarea, fix layout, and update app.py

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 function addRecommendation() {
-  // Get the message of the new recommendation
   let recommendation = document.getElementById("new_recommendation");
-  showPopup(true);
+
   if (recommendation.value != null && recommendation.value.trim() != "") {
     console.log("New recommendation added");
     showPopup(true);
 
-    // Create a new 'recommendation' element and set it's value to the user's message
-    var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-    // Add this element to the end of the list of recommendations
+    let element = document.createElement("div");
+    element.setAttribute("class", "recommendation");
+    element.innerHTML = "<span>&#8220;</span>" + recommendation.value + "<span>&#8221;</span>";
+
     document.getElementById("all_recommendations").appendChild(element); 
-    
-    // Reset the value of the textarea
     recommendation.value = "";
   }
 }
 
 function showPopup(bool) {
+  const popup = document.getElementById('popup');
   if (bool) {
-    document.getElementById('popup').style.visibility = 'visible'
+    popup.style.visibility = 'visible';
+    setTimeout(() => popup.style.visibility = 'hidden', 2000); // Hide after 2 seconds
   } else {
-    document.getElementById('popup').style.visibility = 'hidden'
+    popup.style.visibility = 'hidden';
   }
 }
